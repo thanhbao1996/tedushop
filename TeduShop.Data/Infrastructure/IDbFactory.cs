9b16d0334ad354f6fd88858818e3ba638787409a
Nguyen Thanh Bao <thanhbao9x@gmail.com>
2018-08-10 10:47:21 +0700

    update

---
+++ TeduShop.Data/Infrastructure/IDbFactory.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TeduShop.Data.Infrastructure
+{
+    /// <summary>
+    /// Giao tiep khoi tao cac doi tuong Entity
+    /// </summary>
+    public interface IDbFactory : IDisposable
+    {
+        TeduShopDbContext Init();
+    }
+}
